edeb093ebcfb329ee3907bf78d653ea5b6d597d1
JunedShaikh01 <121929649+JunedShaikh01@users.noreply.github.com>
2023-01-04 13:24:11 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,43 @@
+#pracical 1
+"""class Employee:
+    id=40
+    name="Abhishek"
+    Name="raj"
+    def display(self):
+        print("Id:%d\nName:%s"%(self.id,self.name))
+    def show(abc):
+        print("Id:%d\nName:%s"%(abc.id,abc.Name))
+
+emp=Employee()
+emp.display()
+emp.show()"""
+
+#practical 2
+"""class Number:
+    num=4
+    def display(self):
+        if(self.num%2==0):
+            print("even")
+        else:
+            print("odd")
+emp=Number()
+emp.display()
+"""
+#practical 3
+class calculator:
+    a=int(input("enter a number"))
+    b=int(input("enter a number"))
+    def Add(self):
+        print("the Addition of numbers is",self.a+self.b)
+    def sub(self):
+        print("the subtraction of numbers is",self.a-self.b)
+    def multiply(self):
+        print("the multiplication of numbers is",self.a*self.b)    
+    def div(self):
+        print("the division of numbers is",self.a/self.b)
+emp=calculator()
+emp.Add()
+emp.sub()
+emp.multiply()
+emp.div()
+        
